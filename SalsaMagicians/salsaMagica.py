9a956ea06d99a26a8516018d6bfce02ff6e7be1a
rcasanoval <ricardocasanova99@gmail.com>
2020-09-12 11:02:08 +0200

    SalsaMagicians

---
+++ SalsaMagicians/salsaMagica.py
@@ -0,0 +1,16 @@
+#Esta app servirá para que los magos de Harry Potter toquen Salsa
+characterOne = "Snape"
+characterTwo = "Hermione"
+characterThree = "Hagrid"
+
+#CharacterOne
+if (characterOne == "Snape"):
+  print("Snape toca los bongós")
+elif (characterOne == "Harry"):
+  print("Harry toca la trompeta")
+
+#CharacterTwo
+if (characterOne == "Hermione"):
+	print("Hermione toca la flauta")
+elif (characterOne == "Ron"):
+	print("Ron toca la pandereta")
